686b304321836bf198a0b4d95ede7c109d6915b6
Dmitry Kuzin <dmitry@kuzin.es>
2017-08-25 21:23:03 +0300

    Init

---
+++ ping_pong/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ping_pong
+//
+//  Created by kuzindmitry on 25.08.17.
+//  Copyright © 2017 Dmitry Kuzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+
+@end
+
---
+++ ping_pong/GameViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  ping_pong
+//
+//  Created by kuzindmitry on 25.08.17.
+//  Copyright © 2017 Dmitry Kuzin. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *paddleTop;
+@property (weak, nonatomic) IBOutlet UIImageView *paddleBottom;
+@property (nonatomic) CGFloat halfScreenHeight;
+
+@end
+
+@implementation GameViewController {
+    
+    UITouch *topTouch;
+    UITouch *bottomTouch;
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (CGFloat)halfScreenHeight {
+    return [UIScreen mainScreen].bounds.size.height/2;
+}
+
+#pragma mark - Touches
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint point = [touch locationInView:self.view];
+        if (bottomTouch == nil && point.y < _halfScreenHeight) {
+            bottomTouch = touch;
+            _paddleBottom.center = CGPointMake(point.x, point.y);
+        }
+        else if (topTouch == nil && point.y < _halfScreenHeight) {
+            topTouch = touch;
+            _paddleTop.center = CGPointMake(point.x, point.y);
+        }
+    }
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint point = [touch locationInView:self.view];
+        if (touch == topTouch) {
+            _paddleTop.center = CGPointMake(point.x, point.y);
+        }
+        else if (touch == bottomTouch) {
+            _paddleTop.center = CGPointMake(point.x, point.y);
+        }
+    }
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        if (touch == topTouch) {
+            topTouch = nil;
+        }
+        else if (touch == bottomTouch) {
+            bottomTouch = nil;
+        }
+    }
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self touchesEnded:touches withEvent:event];
+}
+
+@end
