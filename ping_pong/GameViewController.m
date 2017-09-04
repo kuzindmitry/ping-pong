7063a8e756511e15a9a68713eb7a10bbe0f4fa7e
Dmitry Kuzin <dmitry@kuzin.es>
2017-09-04 13:28:51 +0300

    Remove storyboard

--- ping_pong/GameViewController.m
+++ ping_pong/GameViewController.m
@@ -10,9 +10,11 @@
 
 @interface GameViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *paddleTop;
-@property (weak, nonatomic) IBOutlet UIImageView *paddleBottom;
+@property (strong, nonatomic) UIImageView *paddleTop;
+@property (strong, nonatomic) UIImageView *paddleBottom;
+@property (strong, nonatomic) UIView *gridView;
 @property (nonatomic) CGFloat halfScreenHeight;
+@property (nonatomic) CGFloat halfScreenWidth;
 
 @end
 
@@ -38,10 +40,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self config];
+}
+
+- (void)config {
+    self.view.backgroundColor = [UIColor colorWithRed:100.0/255.0 green:135.0/255.0 blue:191.0/255.0 alpha:1.0];
+    
+    _gridView = [[UIView alloc] initWithFrame:CGRectMake(0, self.halfScreenHeight - 2, [UIScreen mainScreen].bounds.size.width, 4)];
+    _gridView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_gridView];
+    
+    _paddleTop = [[UIImageView alloc] initWithFrame:CGRectMake(30, 40, 90, 60)];
+    _paddleTop.image = [UIImage imageNamed:@"paddleTop"];
+    _paddleTop.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:_paddleTop];
+    
+    _paddleBottom = [[UIImageView alloc] initWithFrame:CGRectMake(30, [UIScreen mainScreen].bounds.size.height - 90, 90, 60)];
+    _paddleBottom.image = [UIImage imageNamed:@"paddleBottom"];
+    _paddleBottom.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:_paddleBottom];
 }
 
 - (CGFloat)halfScreenHeight {
     return [UIScreen mainScreen].bounds.size.height/2;
+}
+
+- (CGFloat)halfScreenWidth {
+    return [UIScreen mainScreen].bounds.size.width/2;
 }
 
 #pragma mark - Touches
@@ -64,10 +89,18 @@
     for (UITouch *touch in touches) {
         CGPoint point = [touch locationInView:self.view];
         if (touch == topTouch) {
-            _paddleTop.center = CGPointMake(point.x, point.y);
+            if (point.y > self.halfScreenHeight) {
+                _paddleTop.center = CGPointMake(point.x, self.halfScreenHeight);
+                return;
+            }
+            _paddleTop.center = point;
         }
         else if (touch == bottomTouch) {
-            _paddleBottom.center = CGPointMake(point.x, point.y);
+            if (point.y < self.halfScreenHeight) {
+                _paddleBottom.center = CGPointMake(point.x, self.halfScreenHeight);
+                return;
+            }
+            _paddleBottom.center = point;
         }
     }
 }
